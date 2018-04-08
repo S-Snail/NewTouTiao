2138c9bee42476d4676148625e0c7282185c4cf4
S-Snail <1114979847@qq.com>
2018-04-09 00:48:59 +0800

    增加SplashActivity

---
+++ app/src/main/java/com/example/newtoutiao/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.newtoutiao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 李忠成 on 2018/4/7.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
