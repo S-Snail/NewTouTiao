03380a1f07a10a5b751b13bc399c77b367bd5c2f
S-Snail <1114979847@qq.com>
2018-07-03 00:51:05 +0800

    增加NewsTabLayout布局

--- app/src/main/java/com/example/newtoutiao/module/news/NewsTabLayout.java
+++ app/src/main/java/com/example/newtoutiao/module/news/NewsTabLayout.java
@@ -1,13 +1,18 @@
 package com.example.newtoutiao.module.news;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.newtoutiao.R;
+import com.example.newtoutiao.module.news.channel.NewsChannelActivity;
 
 /**
  * Created by 李忠成 on 2018/4/15.
@@ -17,6 +22,7 @@ public class NewsTabLayout extends Fragment {
 
     public static final String TAG = NewsTabLayout.class.getName();
     private static NewsTabLayout instance = null;
+    private ViewPager viewPager;
 
     public static NewsTabLayout getInstance(){
         if (instance == null){
@@ -30,6 +36,30 @@ public class NewsTabLayout extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news_tab,container,false);
+        initView(view);
+        initData();
         return view;
+    }
+
+    private void initView(View view) {
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout_news);
+        viewPager = view.findViewById(R.id.view_pager_news);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        ImageView add_channel_iv = view.findViewById(R.id.add_channel_iv);
+        add_channel_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),NewsChannelActivity.class));
+            }
+        });
+    }
+
+    private void initData() {
+        initTabs();
+    }
+
+    private void initTabs() {
+
     }
 }
---
+++ app/src/main/java/com/example/newtoutiao/module/news/channel/NewsChannelActivity.java
@@ -0,0 +1,15 @@
+package com.example.newtoutiao.module.news.channel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.newtoutiao.R;
+
+public class NewsChannelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_channel);
+    }
+}
